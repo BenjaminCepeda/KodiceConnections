54c69016129c5d668de5157a93c58c3db012a3a3
Benjamin Cepeda <bcepeda_98@yahoo.com>
2020-12-08 14:52:58 -0500

    Validacion de cierre de conexion

--- src/ec/com/kodice/connection/CustomConnection.java
+++ src/ec/com/kodice/connection/CustomConnection.java
@@ -3,18 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ec.com.kodice.connection;
 
 import java.sql.*;
 
 /**
  * Esta clase permite la conexión a Base de Datos
+ *
  * @author Benjamin Cepeda
  * @version v1.0
- * @date 2020/12/03 
+ * @date 2020/12/03
  */
 public class CustomConnection {
+
     private static String driverClass = "com.mysql.cj.jdbc.Driver";
     private static String hostName = "localhost";
     private static String port = "3306";
@@ -27,27 +28,32 @@ public class CustomConnection {
     private static Connection connection = null;
 
     public static Connection getConnection() throws Exception {
-      if (connection == null) {
-         try {
-            Class.forName(driverClass);
-            connection = DriverManager.getConnection(url, userName, password);
-         } catch (SQLException e) {
-            throw new Exception("["+thisClassName+"] "+e.getMessage());
-         } catch (ClassNotFoundException e) {
-            throw new Exception("["+thisClassName+"] "+e.getMessage());
-         }
-      }
-      return connection;
+        if (connection == null) {
+            try {
+                Class.forName(driverClass);
+                connection = DriverManager.getConnection(url, userName, password);
+            } catch (SQLException e) {
+                throw new Exception("[" + thisClassName + "] " + e.getMessage());
+            } catch (ClassNotFoundException e) {
+                throw new Exception("[" + thisClassName + "] " + e.getMessage());
+            }
+        }
+        return connection;
     }
-    
+
     public static void close() throws Exception {
-      if (connection != null) {
-         try {
-             connection.close();
-         } catch (SQLException e) {
-            throw new Exception("["+thisClassName+"] "+e.getMessage());
+        if (connection != null) {
+            try {
+                if (!connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new Exception("[" + thisClassName + "] " + e.getMessage());
+            }
+            finally{
+                connection = null;                        
+            }
         }
     }
-   }
-    
+
 }
